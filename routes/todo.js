3077e07db0d6391d21f9a8667cf12fa5ecf1e7bd
Pol52 <innocentipaolo1@gmail.com>
2020-12-10 08:39:43 +0100

    simplified db connection

--- routes/todo.js
+++ routes/todo.js
@@ -2,30 +2,25 @@ var express = require('express');
 var router = express.Router();
 var sessionChecker = require('../session');
 var createError = require('http-errors');
-var db = require('../db/db');
- 
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 33061,
+    user: 'todo-user',
+    password: 'todo-user',
+    database: 'todo',
+    multipleStatements: true
+});
+
 router.get('/', sessionChecker, function(req, res, next) {
-    db.query(
-        "SELECT * FROM tasks" +
-        " WHERE completed = false" +
-        " AND userId=" + req.session.user.id, 
-        (err, rows, _fields) => {
-            if(err){
-                next(createError(409));
-            }else{
-                res.json(rows); 
-            }
-        } 
-    )
-}); 
+    db.query('SELECT * FROM tasks WHERE userId=' + req.session.user.id, (err, rows) => handleDBResult(res, next, rows, err));
+});
 
 
 router.post('/', sessionChecker, (req, res, next) => {
-    db.query(
-        "INSERT INTO tasks" +
-        " (userId, completed, task)" +
-        " VALUES (" + req.session.user.id + ", false,'" + req.body.task +"')", 
-        (err, _rows, _fields) => {
+    db.query( 'INSERT INTO tasks (userId, completed, task) VALUES ( ' + req.session.user.id + ', false, "' + req.body.task + '")',
+        (err, _rows) => {
             if(err){
                 next(createError(409));
             }else{
@@ -35,17 +30,16 @@ router.post('/', sessionChecker, (req, res, next) => {
 });
 
 router.patch('/:taskId', sessionChecker, (req, res, next) => {
-    db.query(
-        "UPDATE tasks" +
-        " SET completed = true" +
-        " WHERE id = " + req.params.taskId, 
-        (err, rows, _fields) => {
-            if(err){
-                next(createError(409));
-            }else{
-                res.json(rows);
-            }
-        })
+    db.query( "UPDATE tasks SET completed = true WHERE id = " + req.params.taskId,
+        (err, rows) => handleDBResult(res, next, rows, err))
 })
+
+function handleDBResult(res, next, rows, err){
+    if(err){
+        next(createError(409));
+    }else{
+        res.json(rows);
+    }
+}
 
 module.exports = router;
